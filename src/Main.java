77c4f1e28ad3bc04e8cb65fe12da0fbbceea6030
GediminasGsn <gasiunasgediminas@gmail.com>
2024-03-20 21:18:38 +0200

    ***

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Scanner scn = new Scanner(System.in);
+    double fnum, snum, answer;
+        System.out.println("Enter first nuber:");
+        fnum = scn.nextDouble();
+        System.out.println("Enter second number:");
+        snum = scn.nextDouble();
+        answer = fnum + snum;
+        System.out.println("Equals to:" + (int) answer);
+
     }
 }
